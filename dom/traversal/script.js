d6c539431052ed3732b69faab87da0061185883a
denanda prayoga <denanda79@gmail.com>
2021-05-14 16:42:21 +0700

    event bubbling

--- dom/traversal/script.js
+++ dom/traversal/script.js
@@ -1,10 +1,30 @@
 //DOM Traversal
-const close = document.querySelectorAll('.close');
-close.forEach((el) => {
-  el.addEventListener('click', function (e) {
+// const close = document.querySelectorAll('.close');
+// close.forEach((el) => {
+//   el.addEventListener('click', function (e) {
+//     e.target.parentElement.style.display = 'none';
+
+//preventDefault() menghilangkan perilaku default nya
+// e.preventDefault();
+
+//menghentikan event bubbling
+// e.stopPropagation();
+//   });
+// });
+
+// const cards = document.querySelectorAll('.card');
+// cards.forEach((card) => {
+//   card.addEventListener('click', function () {
+//     alert('oke');
+//   });
+// });
+
+const container = document.querySelector('.container');
+container.addEventListener('click', function (e) {
+  if (e.target.className === 'close') {
     e.target.parentElement.style.display = 'none';
 
-    //preventDefault() menghilangkan perilaku default nya
+    //mengatasi aksi default
     e.preventDefault();
-  });
+  }
 });
