f1f222ae58a81c4e6be67a520a6902bb58c50d81
denanda prayoga <denanda79@gmail.com>
2021-05-29 21:51:56 +0700

    css unit calc

--- study cases/angkot/script.js
+++ study cases/angkot/script.js
@@ -1,22 +1,22 @@
 function Angkot(sopir, trayek, penumpang, kas) {
-  this.sopir = sopir;
-  this.trayek = trayek;
-  this.penumpang = penumpang;
-  this.kas = kas;
-  this.penumpangNaik = function (namaPenumpang) {
-    this.penumpang.push(namaPenumpang);
-  };
-  this.penumpangTurun = function (namaPenumpang, bayar) {
-    if (penumpang.length === 0) {
-      console.log(`angkot masih kosong`);
-      return penumpang;
-    }
-    for (let i = 0; i < penumpang.length; i++) {
-      this.penumpang.splice(i, 1);
-      this.kas += bayar;
-      return penumpang;
-    }
-  };
+    this.sopir = sopir;
+    this.trayek = trayek;
+    this.penumpang = penumpang;
+    this.kas = kas;
+    this.penumpangNaik = function(namaPenumpang) {
+        this.penumpang.push(namaPenumpang);
+    };
+    this.penumpangTurun = function(namaPenumpang, bayar) {
+        if (penumpang.length === 0) {
+            console.log(`angkot masih kosong`);
+            return penumpang;
+        }
+        for (let i = 0; i < penumpang.length; i++) {
+            this.penumpang.splice(i, 1);
+            this.kas += bayar;
+            return penumpang;
+        }
+    };
 }
 
 let angkot1 = new Angkot("aden", ["citayam", "bojonggede"], [], 0);
